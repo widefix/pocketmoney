fdafea406e68930a65e64f19fb6b58962ae8b7dd
Andrey Koleshko <ka8725@gmail.com>
2021-11-19 23:59:26 +0100

    slim and account page view

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'devise'
 gem "bulma-rails", "~> 0.9.2"
+gem 'slim'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,6 +5,10 @@ class Transaction < ApplicationRecord
   belongs_to :from_account, class_name: 'Account', foreign_key: :from_account_id
 
   def to_s
-    "#{created_at.to_date} #{from_account.name} -> #{to_account.name}: #{amount}"
+    "#{date} #{from_account.name} -> #{to_account.name}: #{amount}"
+  end
+
+  def date
+    created_at.to_date
   end
 end

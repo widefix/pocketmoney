75328af47073298518009ef2eed27eb7a135d2e9
Andrey Koleshko <ka8725@gmail.com>
2022-03-25 00:18:54 +0100

    fix host for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'https://pocketmoneyapp.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'pocketmoneyapp.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     port:           ENV['MAILGUN_SMTP_PORT'],
     address:        ENV['MAILGUN_SMTP_SERVER'],

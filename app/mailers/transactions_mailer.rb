ba749845715b21c88525dbe80ceb6a0c2a472c13
koleshkos <koleshkoser@gmail.com>
2022-09-10 17:55:58 +0300

    send message to email if add tr

--- app/mailers/transactions_mailer.rb
+++ app/mailers/transactions_mailer.rb
@@ -9,7 +9,9 @@ class TransactionsMailer < ApplicationMailer
   end
 
   def transaction_notification(account)
+    @account = account
     @transaction = Transaction.last
-    mail to: account.email, subject: "Success! Transaction added." 
+    @from_account = @transaction.from_account.name
+    mail to: @account.email, subject: "Success! Transaction added." 
   end
 end

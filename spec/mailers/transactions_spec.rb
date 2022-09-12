8f3dd185dd33ffd628cb75b73332537f09e8acc5
koleshkos <koleshkoser@gmail.com>
2022-09-12 13:38:12 +0300

    update mail specs

--- spec/mailers/transactions_spec.rb
+++ spec/mailers/transactions_spec.rb
@@ -1,17 +1,22 @@
 require "rails_helper"
 
-RSpec.describe TransactionsMailer, :type => :mailer do
+RSpec.describe TransactionsMailer, type: :mailer do
   describe "notify" do
     let(:parent) { create(:account, :parent) }
-    let(:child) { create(:account, :children, parent_id: parent.id ) }
-    let!(:transaction) { create(:transaction, to_account_id: child.id, from_account_id: parent.id) }
+    let(:child) { create(:account, :children, parent: parent ) }
+    let!(:transaction) { create(:transaction, to_account: child, from_account: parent) }
 
-    subject { TransactionsMailer.transaction_notification(child).deliver }
+    subject(:mail) { TransactionsMailer.transaction_notification(child).deliver }
 
-    it { expect { subject }.to change { ActionMailer::Base.deliveries.count }.by(1) }
-    it { expect(subject.to).to eq([child.email]) }
-    it { expect(subject.from).to eq(["no-reply@widefix.com"]) }
-    it { expect(subject.subject).to eq("Transaction added.") }
-    it { expect(subject.body.encoded).to match(child.name) }
+    it 'recipient must be correct' do
+      expect(mail).to eq(ActionMailer::Base.deliveries.find { |mail| mail.to.include?(child.email) })
+    end
+    
+    it { expect { mail }.to change { ActionMailer::Base.deliveries.count }.by(1) }
+    it { expect(mail.to).to eq([child.email]) }
+    it { expect(mail.from).to eq(["no-reply@widefix.com"]) }
+    it { expect(mail.subject).to eq("Transaction added.") }
+    it { expect(mail.body.encoded).to match(child.name) }
+
   end
 end

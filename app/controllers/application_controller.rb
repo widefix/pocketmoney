2b8bfc3bb163c4a0346ab1d950a7efd7416d909d
koleshkos <koleshkoser@gmail.com>
2022-09-15 18:02:36 +0300

    fix space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
   private
 
   helper_method memoize def account
-
     Account.visible_for(current_user).find(ps.fetch(:account_id))
   end
 end
--- app/controllers/topups_controller.rb
+++ app/controllers/topups_controller.rb
@@ -3,7 +3,6 @@ class TopupsController < ApplicationController
   end
 
   def create
-
     Transaction.create!(
       from_account: current_user.account,
       to_account: account,
@@ -14,7 +13,7 @@ class TopupsController < ApplicationController
     if account.notification?
       TransactionsMailer.transaction_notification(account).deliver
     end
-    
+
     redirect_to account_path(account)
   end
 end

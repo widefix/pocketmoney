64c24e431945136c92c207393f098efb6ffe0686
Vlad Sokov <vladilsav.sokov.92@gmail.com>
2023-08-18 11:08:28 +0200

    feat(account_invitation): add foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :account_invitations
+  has_many :account_invitations, foreign_key: :user_id
   belongs_to :account, optional: true
 end

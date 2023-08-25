2fa289efa89a325f9a67dc08d516689d4c08324d
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2023-08-25 16:50:07 +0300

    feat(account invitation): abstain from the conditional assignment

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -13,9 +13,8 @@ module Users
     private
 
     def after_sign_in_path_for(_resource)
-      if (after_sign_in_url = session.delete(:after_sign_in_url)).present?
-        return after_sign_in_url
-      end
+      after_sign_in_url = session.delete(:after_sign_in_url)
+      return after_sign_in_url if after_sign_in_url.present?
 
       super
     end

5d477c47b2a05a6d21af4a0ddb676f19b316daa2
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2023-08-30 16:49:15 +0300

    feat(user): use select, fix tests

--- spec/mailers/invitations_spec.rb
+++ spec/mailers/invitations_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe InvitationMailer, type: :mailer do
     let(:invitee_user) { create(:user) }
     let!(:account_invitation) { create(:account_invitation, user: user, account: account, email: invitee_user.email) }
 
-    subject(:mail) { InvitationMailer.account_invitation(account_invitation, user).deliver }
+    subject(:mail) { InvitationMailer.account_invitation(account_invitation).deliver }
 
     it { expect(mail.to).to eq([invitee_user.email]) }
     it { expect(mail.subject).to eq("Invitation to manage an account from #{user.name || "#{user.email} owner"}") }

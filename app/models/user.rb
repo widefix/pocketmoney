b5b8170e3043e1f0afabeaefddbded3195b39032
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2023-08-30 23:48:36 +0300

    feat(user): add user name (#39)
    * Add user name
    
    and update the app in necessary places
    
    * feat(user) some changes
    
    * feat(user): change mailer parameters
    
    * feat(user): use select, fix tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :account, optional: true
+
+  validates :name, allow_blank: true, length: { minimum: 2, maximum: 20 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2023_08_30_131000) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "account_id"
+    t.string "name"
     t.index ["account_id"], name: "index_users_on_account_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

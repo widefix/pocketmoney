6bc07614284bd2b0f0cd54796f8bc0dca6ee3243
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2023-08-31 13:25:57 +0300

    feat(user profile): fix to the private access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,6 @@ class ApplicationController < ActionController::Base
     Account.visible_for(current_user).find(ps.fetch(:account_id))
   end
 
-  protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end

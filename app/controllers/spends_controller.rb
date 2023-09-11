a32d31da27d1b863bd9bdf717b9989b751616ac7
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2023-09-11 23:04:57 +0300

    feat(notification): update according to the comments

--- app/controllers/spends_controller.rb
+++ app/controllers/spends_controller.rb
@@ -11,17 +11,8 @@ class SpendsController < ApplicationController
       originator: current_user,
       access_token: Devise.friendly_token
     )
-    send_notification
+    SendNotification.call(account)
 
     redirect_to account_path(account)
-  end
-
-  def send_notification
-    return unless account.notification?
-
-    TransactionsMailer.transaction_notification(account).deliver
-    account.account_shares.accepted.each do |account_share|
-      TransactionsMailer.transaction_notification(account, account_share).deliver
-    end
   end
 end

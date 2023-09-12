d0ac9696dc5b08585126d04217dd0f5cadb00c25
Vladislav Sokov <62177364+VladislavSokov@users.noreply.github.com>
2023-09-12 19:18:13 +0300

    feat(expo): add expo (#94)
    * feat(expo): add expo
    
    * feat(expo): fixup! add expo
    
    * feat(expo): fixup! fixup! add expo
    
    * feat(expo): fixup! fixup! fixup! add expo
    
    ---------
    
    Co-authored-by: VladislavSokov <vladilsav.sokov.92@gmail.com>

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import { WebView } from 'react-native-webview';
+import Config from './config';
+
+export default function App() {
+  return (
+    <WebView
+      style={{ marginTop: 25 }}
+      source={{ uri: Config.apiUrl }} 
+    />
+  );
+}

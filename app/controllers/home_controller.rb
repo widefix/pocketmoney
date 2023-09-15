5ef7584ef5a563897295aed50f33b0b7d45d1b60
Vladislav Sokov <62177364+VladislavSokov@users.noreply.github.com>
2023-09-15 19:17:48 +0300

    feat(capybara): configure a capybara and add home_page scenario (#99)
    * feat(capybara): configure a capybara and add home_page scenario
    
    * feat(capybara): fixup! configure a capybara and add home_page scenario
    
    * feat(capybara): fixup! fixup! configure a capybara and add home_page scenario
    
    * feat(capybara): fixup! fixup! fixup! configure a capybara and add home_page scenario
    
    * feat(capybara): update nodejs
    
    * feat(capybara): fixup! update nodejs
    
    * feat(capybara): fixup! update nodejs
    
    * feat(capybara): fixup! update nodejs
    
    ---------
    
    Co-authored-by: VladislavSokov <vladilsav.sokov.92@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
     @pocket_money_reasons = YAML.load_file(Rails.root.join('config', 'pocket_money_reasons.yml'))['reasons']
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'database_cleaner'
+require 'capybara/rspec'
+require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -37,6 +39,8 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+
+  config.include Capybara::DSL
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false

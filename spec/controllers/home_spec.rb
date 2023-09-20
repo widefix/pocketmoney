3419e592ce39fd9bbc319a41e8967ed43a101fc7
Vladislav Sokov <62177364+VladislavSokov@users.noreply.github.com>
2023-09-20 13:11:40 +0300

    feat(home page): redirect to my account when a user is logged in (#117)

--- spec/controllers/home_spec.rb
+++ spec/controllers/home_spec.rb
@@ -1,12 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe HomeController do
   describe '#index' do
-    
     subject { get :index }
 
-    it { is_expected.to have_http_status(:success) }
-    it { is_expected.to render_template(:index) }
-    it { is_expected.to_not render_template('accounts/new')}
+    context 'when a user is logged in' do
+      let(:user) { create(:user) }
+
+      before { sign_in(user) }
+
+      it { is_expected.to have_http_status(:redirect) }
+      it { is_expected.to redirect_to(my_account_path) }
+    end
+
+    context 'when no user is logged in' do
+      it { is_expected.to have_http_status(:success) }
+      it { is_expected.to render_template(:index) }
+    end
   end
 end

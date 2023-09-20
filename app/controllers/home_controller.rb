2eb99ddb9ab3de2981f430adbdd693aabf974460
VladislavSokov <vladilsav.sokov.92@gmail.com>
2023-09-20 12:56:48 +0300

    feat(home page): redirect to my account when a user is logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@
 
 class HomeController < ApplicationController
   def index
+    redirect_to my_account_path if user_signed_in?
+
     @budgeting_kid_reasons = YAML.load_file(Rails.root.join('config', 'budgeting_kid_reasons.yml'))['reasons']
   end
 end

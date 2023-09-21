07c1d1de6931829f296dc6897a844e7a07863a93
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2023-09-21 15:29:21 +0300

    feat(mobile): adapte navigation

--- app/javascript/menu/navbar.js
+++ app/javascript/menu/navbar.js
@@ -1,18 +1,50 @@
 document.addEventListener('turbolinks:load', () => {
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-      });
-    });
+  if (window.innerWidth > 768) {
+    return
   }
-});
+  const navbarBurgers = document.querySelector('.navbar-burger')
+  const navbar = document.querySelector('.navbar')
+  const footer = document.querySelector('.navbar-footer')
+  const hideSpeedDown = 15
+  const showSpeedUp = 25
+  var lastScrollPisition = 0
+  var isClicked = false
+  var isBottomed = false
+
+  window.addEventListener('scroll', () => {
+    var scrollTop = window.scrollY || document.documentElement.scrollTop
+    var scrollSpeedDown = scrollTop - lastScrollPisition
+    var scrollSpeedUp = lastScrollPisition - scrollTop
+
+    // go down
+    if ((scrollTop > lastScrollPisition && scrollSpeedDown > hideSpeedDown && !isClicked) || isBottomed) {
+      navbar.style.transform = 'translateY(-100%)'
+      footer.style.opacity = '0.4'
+      // go up
+    } else if (scrollTop < lastScrollPisition && scrollSpeedUp > showSpeedUp) {
+      navbar.style.transform = 'translateY(0)'
+      footer.style.opacity = '1'
+      // at the top
+    } else if (scrollTop === 0) {
+      navbar.style.transform = 'translateY(0)'
+      footer.style.opacity = '1'
+    }
+    isBottomed = false
+    // at the bottom
+    if ((window.innerHeight + scrollTop) >= document.body.scrollHeight) {
+      navbar.style.transform = 'translateY(0)'
+      footer.style.opacity = '1'
+      isBottomed = true
+    }
+    lastScrollPisition = scrollTop <= 0 ? 0 : scrollTop
+    isClicked = false
+  }, false)
+
+  navbarBurgers.addEventListener('click', () => {
+    isClicked = true
+    const target = document.getElementById(navbarBurgers.dataset.target)
+
+    navbarBurgers.classList.toggle('is-active')
+    target.classList.toggle('is-active')
+  })
+})

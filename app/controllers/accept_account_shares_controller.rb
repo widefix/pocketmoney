dbc7d1fd7fb7c3ad7dbc20aa777cefed024ae6af
Vladislav Sokov <62177364+VladislavSokov@users.noreply.github.com>
2023-09-25 14:13:30 +0300

    fix accept account share (#123)
    * fix accept account share
    
    * fixup! fix accept account share
    
    * add tests
    
    * fixup! add tests
    
    ---------
    
    Co-authored-by: VladislavSokov <vladilsav.sokov.92@gmail.com>

--- app/controllers/accept_account_shares_controller.rb
+++ app/controllers/accept_account_shares_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class AcceptAccountSharesController < ApplicationController
+  before_action :received_share, only: [:show], if: -> { user_signed_in? }
+
   def show
     return if user_signed_in?
 
     session[:after_sign_in_url] = request.fullpath
 
-    shared_email = AccountShare.find_by!(token: ps.fetch(:token)).email
+    shared_email = AccountShare.unaccepted.find_by!(token: ps.fetch(:token)).email
     user = User.find_by(email: shared_email)
     redirect_url = user ? new_user_session_url(email: shared_email) : new_user_registration_url(email: shared_email)
 

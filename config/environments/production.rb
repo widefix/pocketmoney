0a170c788fe235e9e3a68aa2bbe52b5a6b189799
VladislavSokov <vladilsav.sokov.92@gmail.com>
2023-09-27 21:05:31 +0300

    fix(:reset password) add https protocol for default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'budgetingkid.com' }
+  config.action_mailer.default_url_options = { host: 'budgetingkid.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: ENV['SMTP_PORT'],

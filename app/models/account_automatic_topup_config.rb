1ae77176e491c72c161f67ebfbe052ffdf072ac1
Andrei Kaleshka <ka8725@gmail.com>
2023-10-01 16:47:25 +0200

    fix top-up select

--- app/models/account_automatic_topup_config.rb
+++ app/models/account_automatic_topup_config.rb
@@ -2,5 +2,5 @@ class AccountAutomaticTopupConfig < ApplicationRecord
   belongs_to :from_account, class_name: 'Account', foreign_key: :from_account_id
   belongs_to :to_account, class_name: 'Account', foreign_key: :to_account_id
 
-  scope :visible, -> { where(to_account: { archived_at: nil }) }
+  scope :visible, -> { joins(:to_account).where(to_account: { archived_at: nil }) }
 end

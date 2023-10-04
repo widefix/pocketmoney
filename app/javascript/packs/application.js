a939086a312f770ab42da2de29fc7a7b2d5507b8
Vladislav Sokov <62177364+VladislavSokov@users.noreply.github.com>
2023-10-04 22:48:51 +0300

    feat(all balance): add all balance button for the new spend form (#177)
    * feat(all balence): add all balance button for the new spend form
    
    * feat(all balence): fixup! add all balance button for the new spend form
    
    * feat(all balence): fixup! fixup! add all balance button for the new spend form
    
    * feat(all balence): fixup! fixup! fixup! add all balance button for the new spend form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("channels")
 require("menu/navbar")
 require("menu/dropdowns")
 require("account/toggleButton")
+require("spends/allBalance")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
---
+++ app/javascript/spends/allBalance.js
@@ -0,0 +1,16 @@
+document.addEventListener('turbolinks:load', function() {
+  const allBalanceButton = document.querySelector('.all-balance-button');
+  
+  if (allBalanceButton) {
+    allBalanceButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      const balance = parseFloat(this.getAttribute('data-balance'));
+
+      const amountInput = document.querySelector('.field .amount');
+      amountInput.value = balance;
+      const descriptionInput = document.querySelector('.field .decscription');
+      descriptionInput.value = 'Spend all balance';
+      window.history.pushState({}, document.title, window.location.href);
+    });
+  }
+});

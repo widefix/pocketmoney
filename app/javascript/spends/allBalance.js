5391d964e7655e9eb8ebb9dde37cafae793cc61e
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2023-10-04 18:51:35 +0300

    feat(all balence): fixup! add all balance button for the new spend form

--- app/javascript/spends/allBalance.js
+++ app/javascript/spends/allBalance.js
@@ -1,14 +1,14 @@
 document.addEventListener('turbolinks:load', function() {
-  var allBalanceButton = document.querySelector('.all-balance-button');
+  const allBalanceButton = document.querySelector('.all-balance-button');
   
   if (allBalanceButton) {
     allBalanceButton.addEventListener('click', function(event) {
       event.preventDefault();
-      var balance = parseFloat(this.getAttribute('data-balance'));
+      const balance = parseFloat(this.getAttribute('data-balance'));
 
-      var amountInput = document.querySelector('.field .amount');
+      const amountInput = document.querySelector('.field .amount');
       amountInput.value = balance;
-      var descriptionInput = document.querySelector('.field .decscription');
+      const descriptionInput = document.querySelector('.field .decscription');
       descriptionInput.value = 'Spend all balance';
     });
   }

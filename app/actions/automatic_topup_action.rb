e87c873692ec65a87ed0c5b17fd446bfe3fbece1
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2023-10-06 15:08:51 +0300

    fixup! fix automatic topup

--- app/actions/automatic_topup_action.rb
+++ app/actions/automatic_topup_action.rb
@@ -8,12 +8,28 @@ class AutomaticTopupAction < ApplicationAction
   private
 
   def perform_implementation
-    transaction = Transaction.create!(
+    return if existing_transaction
+
+    transaction = Transaction.new(
       from_account: from,
       to_account: to,
       amount: amount,
       description: 'Automatic Top-up'
     )
+
+    return unless transaction.valid?
+
+    transaction.save
     TransactionsMailer.automatic_top_up_done(transaction).deliver_now
+  end
+
+  def existing_transaction
+    Transaction.find_by(
+      from_account: from,
+      to_account: to,
+      amount: amount,
+      description: 'Automatic Top-up',
+      created_at: Date.current.beginning_of_day..Date.current.end_of_day
+    )
   end
 end

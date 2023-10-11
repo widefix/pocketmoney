378904b2e7cedbc58485fe585437712b3db28ccb
Vladislav Sokov <62177364+VladislavSokov@users.noreply.github.com>
2023-10-11 16:33:22 +0300

    feat(wizard): add after signup wizard (#186)
    * feat(wizard): add after signup wizard
    
    * feat(wizard): fixup! add after signup wizard
    
    * feat(wizard): fixup! fixup! add after signup wizard
    
    * feat(wizard): fixup! fixup! fixip! add after signup wizard
    
    * feat(wizard): fixup! fixup! fixip! fixup! add after signup wizard
    
    * feat(wizard): fixup! fixup! fixip! fixup! fixup! add after signup wizard
    
    * feat(wizard): fixup! fixup! fixip! fixup! fixup! fixup! add after signup wizard
    
    * feat(wizard): fixup! fixup! fixip! fixup! fixup! fixup! fixup! add after signup wizard
    
    * feat(wizard): fixup! fixup! fixip! fixup! fixup! fixup! fixup! fixup! add after signup wizard
    
    * feat(wizard): fixup! fixup! fixip! fixup! fixup! fixup! fixup! fixup! fixup! add after signup wizard
    
    * feat(wizard): fixup! fixup! fixip! fixup! fixup! fixup! fixup! fixup! fixup! fixup! add after signup wizard
    
    * feat(wizard): fixup! fixup! fixip! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! add after signup wizard
    
    * fix title
    
    ---------
    
    Co-authored-by: Andrei Kaleshka <ka8725@gmail.com>

---
+++ app/controllers/wizards_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class WizardsController < ApplicationController
+  def new_account; end
+
+  def new_automatic_topup; end
+
+  def new_objective; end
+
+  def new_share_account; end
+
+  def create_account
+    Account.create!(parent: current_user.account, **ps.slice(:name))
+
+    redirect_to new_automatic_topup_wizard_path
+  end
+
+  def create_automatic_topup
+    AccountAutomaticTopupConfig.create!(from_account: current_user.account,
+                                        to_account: account,
+                                        **ps.slice(:amount))
+
+    redirect_to new_objective_wizard_path
+  end
+
+  def create_objective
+    account.objectives.create!(ps.slice(:name, :amount))
+
+    redirect_to new_share_account_wizard_path
+  end
+
+  def create_share_account
+    account_share = AccountShare.create!(user_id: current_user.id,
+                                         account_id: account.id,
+                                         token: SecureRandom.urlsafe_base64(32),
+                                         **ps.slice(:name, :email))
+
+    AccountShareMailer.account_share(account_share).deliver
+
+    redirect_to account_path(account)
+  end
+
+  private
+
+  helper_method memoize def account
+    current_user.account.children.first
+  end
+end
---
+++ spec/controllers/wizards_controller_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe WizardsController, type: :controller do
+  let!(:account) { create(:account, :parent) }
+  let!(:user) { create(:user, account: account) }
+
+  before { sign_in user }
+
+  describe '#new_account' do
+    subject(:new_account) { get :new_account }
+
+    it { is_expected.to have_http_status(:success) }
+    it { is_expected.to render_template(:new_account) }
+  end
+
+  describe '#new_automatic_topup' do
+    subject(:new_automatic_topup) { get :new_automatic_topup }
+
+    it { is_expected.to have_http_status(:success) }
+    it { is_expected.to render_template(:new_automatic_topup) }
+  end
+
+  describe '#new_objective' do
+    subject(:new_objective) { get :new_objective }
+
+    it { is_expected.to have_http_status(:success) }
+    it { is_expected.to render_template(:new_objective) }
+  end
+
+  describe '#new_share_account' do
+    subject(:new_share_account) { get :new_share_account }
+
+    it { is_expected.to have_http_status(:success) }
+    it { is_expected.to render_template(:new_share_account) }
+  end
+
+  describe '#create_account' do
+    subject(:create_account) { post :create_account, params: { name: FFaker::Name.first_name } }
+
+    it { expect { subject }.to change { Account.count }.by(1) }
+    it { is_expected.to redirect_to(new_automatic_topup_wizard_path) }
+  end
+
+  describe '#create_automatic_topup' do
+    let!(:child) { create(:account, :children, parent: account) }
+
+    subject(:create_automatic_topup) { post :create_automatic_topup, params: { amount: FFaker::Number.number } }
+
+    it { expect { subject }.to change { AccountAutomaticTopupConfig.count }.by(1) }
+    it { is_expected.to redirect_to(new_objective_wizard_path) }
+  end
+
+  describe '#create_objective' do
+    let!(:child) { create(:account, :children, parent: account) }
+
+    subject(:create_objective) do
+      post :create_objective, params: { name: FFaker::Book.title, amount: FFaker::Number.number }
+    end
+
+    it { expect { subject }.to change { Objective.count }.by(1) }
+    it { is_expected.to redirect_to(new_share_account_wizard_path) }
+  end
+
+  describe '#create_share_account' do
+    let!(:child) { create(:account, :children, parent: account) }
+
+    subject(:create_share_account) do
+      post :create_share_account, params: { name: FFaker::Book.title, email: FFaker::Internet.email }
+    end
+
+    it { expect { subject }.to change { AccountShare.count }.by(1) }
+    it { is_expected.to redirect_to(account_path(child)) }
+  end
+end

80b2d4d7bb10ca440264ec57bd9b1e6914c62689
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2023-10-19 18:41:24 +0300

    feat(goals notification): fixup! fixup! fixup! fixup! add goals almost achieved notification

--- db/migrate/20231017134708_add_notified_for_objectivites.rb
+++ db/migrate/20231017134708_add_notified_for_objectivites.rb
@@ -2,7 +2,7 @@
 
 class AddNotifiedForObjectivites < ActiveRecord::Migration[6.1]
   def change
-    add_column :objectives, :goal_achieved_notified_at, :datetime, default: nil
-    add_column :objectives, :goal_almost_achieved_notified_at, :datetime, default: nil
+    add_column :objectives, :goal_achieved_notified_at, :datetime
+    add_column :objectives, :goal_almost_achieved_notified_at, :datetime
   end
 end

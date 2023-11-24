b7beda92c1331972502e5fba94666857b8432e1f
Vladislav Sokov <62177364+VladislavSokov@users.noreply.github.com>
2023-11-24 19:44:19 +0300

    Update app/controllers/my_accounts_controller.rb
    Co-authored-by: Andrei Kaleshka <ka8725@gmail.com>

--- app/controllers/my_accounts_controller.rb
+++ app/controllers/my_accounts_controller.rb
@@ -4,9 +4,7 @@ class MyAccountsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    return if current_user.parent?
-
-    redirect_to account_path(shared_accounts.first)
+    redirect_to account_path(shared_accounts.first) unless current_user.parent?
   end
 
   private

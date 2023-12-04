1c2c105b1bac9a0cd68bc44b734d4fcdcd27e352
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2023-12-04 16:31:52 +0300

    fix(change amount): fix uncaught TypeError

--- app/javascript/menu/change_amount.js
+++ app/javascript/menu/change_amount.js
@@ -5,36 +5,38 @@ document.addEventListener('turbolinks:load', function () {
     const allBalanceButton = document.querySelector('.all-balance-button');
     const amountInput = document.querySelector('.field .amount');
 
-    if (!(topupButtons && spendButtons)) {
-        return
-    }
     const modalTopupForm = document.querySelector('#modal-topup-form')
     const modalSpendForm = document.querySelector('#modal-spend-form')
 
     const topupForm = document.getElementById('topup-form-with')
     const spendForm = document.getElementById('spend-form-with')
 
-
-    topupButtons.forEach(function (changeButton) {
-        changeButton.addEventListener('click', function () {
-            modalTopupForm.classList.add('is-active')
-            topupForm.action = changeButton.dataset.url
+    if (topupButtons) {
+        topupButtons.forEach(function (changeButton) {
+            changeButton.addEventListener('click', function () {
+                modalTopupForm.classList.add('is-active')
+                topupForm.action = changeButton.dataset.url
+            })
         })
-    })
+    }
 
-    spendButtons.forEach(function (changeButton) {
-        changeButton.addEventListener('click', function () {
-            modalSpendForm.classList.add('is-active')
-            spendForm.action = changeButton.dataset.url
-            allBalanceButton.dataset.balance = changeButton.dataset.balance
+    if (spendButtons) {
+        spendButtons.forEach(function (changeButton) {
+            changeButton.addEventListener('click', function () {
+                modalSpendForm.classList.add('is-active')
+                spendForm.action = changeButton.dataset.url
+                allBalanceButton.dataset.balance = changeButton.dataset.balance
+            })
         })
-    })
-
-    allBalanceButton.addEventListener('click', function (event) {
-        event.preventDefault();
-        amountInput.value = allBalanceButton.dataset.balance;
-        window.history.pushState({}, document.title, window.location.href);
-    });
+    }
+    
+    if (allBalanceButton) {
+        allBalanceButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            amountInput.value = allBalanceButton.dataset.balance;
+            window.history.pushState({}, document.title, window.location.href);
+        });
+    }
 
     function closeModal(e) {
         e.classList.remove('is-active')
@@ -44,8 +46,10 @@ document.addEventListener('turbolinks:load', function () {
     (document.querySelectorAll('.modal-background, .modal-close, .modal-card .close-button') || []).forEach((close) => {
         const target = close.closest('.modal')
 
-        close.addEventListener('click', () => {
-            closeModal(target)
-        })
+        if (close && target) {
+            close.addEventListener('click', () => {
+                closeModal(target)
+            })
+        }
     })
 })

1a9202afb19044c0ed362d87f7b5d235b2d1af39
Andrei Kaleshka <ka8725@gmail.com>
2023-12-08 00:32:08 +0100

    fix redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@
 
 class HomeController < ApplicationController
   def index
-    redirect_to my_account_path if user_signed_in?
-
-    @budgeting_kid_reasons = YAML.load_file(Rails.root.join('config', 'budgeting_kid_reasons.yml'))['reasons']
+    redirect_to my_account_path
   end
 end

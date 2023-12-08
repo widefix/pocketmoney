45c4781bbb6535b0402f4d5b59a84e3def688bb6
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2023-12-08 17:09:04 +0300

    fix(choose role) fix choose role ui

--- app/controllers/choose_role_controller.rb
+++ app/controllers/choose_role_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class ChooseRoleController < ApplicationController
-  def index; end
+  layout 'initial_application'
+
+  def index
+    redirect_to my_account_path if user_signed_in?
+  end
 end
--- spec/controllers/choose_role_spec.rb
+++ spec/controllers/choose_role_spec.rb
@@ -2,20 +2,21 @@
 
 require 'rails_helper'
 
-RSpec.describe HomeController do
+RSpec.describe ChooseRoleController, type: :controller do
   describe '#index' do
-    subject { get :index }
+    subject(:index) { get :index }
 
-    context 'when a user is logged in' do
-      let(:user) { create(:user) }
+    context 'when user is signed in' do
+      let!(:account) { create(:account, :parent) }
+      let!(:user) { create(:user, account: account) }
 
-      before { sign_in(user) }
+      before { sign_in user }
 
       it { is_expected.to have_http_status(:redirect) }
       it { is_expected.to redirect_to(my_account_path) }
     end
 
-    context 'when no user is logged in' do
+    context 'when user is not signed in' do
       it { is_expected.to have_http_status(:success) }
       it { is_expected.to render_template(:index) }
     end

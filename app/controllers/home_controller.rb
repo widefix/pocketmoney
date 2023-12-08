17d6d2187da13ae81291c4b4412d35f4a6db3420
Vladislav Sokov <62177364+VladislavSokov@users.noreply.github.com>
2023-12-09 00:37:41 +0300

    fix(choose role) fix choose role ui (#321)
    * fix(choose role) fix choose role ui
    
    * fix(choose role) !fixup fix choose role ui
    
    * fix(choose role) fixup! fixup! fix choose role ui

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class HomeController < ApplicationController
-  def index
-    redirect_to my_account_path
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'choose_role#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
